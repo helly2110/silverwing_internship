55452d156584a6201dd659cc274523332cd03d36
helly2110 <hbpatel2110@gmail.com>
2021-05-27 12:04:40 +0530

    home is created

--- internship/internship/settings.py
+++ internship/internship/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-llwm_t#jvlx@58y&#_4+h%i+mjb_n8qxz=fjsp0ohkfj#uf7yu'
+SECRET_KEY = 'django-insecure-ol6ajda1%pev=4&q=sc^p77z#ui!&asmcrifp3rehi#^b_-^9g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
